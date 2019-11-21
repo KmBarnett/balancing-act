82c1b1186ceb814dff870bcec8e79e958cc47e90
kmbarnett <kmbarnett94@gmail.com>
2019-11-21 08:37:45 -0700

    Add id to accounts and js to cause section growth

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let button = document.getElementById('cbut');
 let hide = document.getElementById('hulk');
   function handleClick() {
     hide.style.display='none';
+    epps.style.height='70%';
   }
 button.addEventListener("click", handleClick);
 }());
