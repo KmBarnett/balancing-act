29072bdaa109e6cb9a93f12b8c81150482d8f839
kmbarnett <kmbarnett94@gmail.com>
2019-11-23 12:10:20 -0700

    Add nav button functanality for swaping pages, Add new pages.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// NOTE: banner functionality
 (function() {
 let button = document.getElementById('cbut');
 let hide = document.getElementById('hulk');
@@ -9,27 +10,40 @@ let account = document.getElementById('epps');
   }
 button.addEventListener("click", handleClick);
 }());
-
+// NOTE: Nav functionality
 (function() {
  let btn1 = document.getElementById('btn1')
  let btn2 = document.getElementById('btn2')
  let btn3 = document.getElementById('btn3')
+ let dashboard = document.getElementById('dshbrd')
+ let wallet = document.getElementById('wlt')
+ let user = document.getElementById('usr')
 
  function handleNavClick(butn) {
    if (butn === "1") {
      btn1.classList.add("nav-slctd")
      btn2.classList.remove("nav-slctd")
      btn3.classList.remove("nav-slctd")
+     dashboard.classList.remove("hide")
+     wallet.classList.add("hide")
+     user.classList.add("hide")
+
    }
    else if (butn === "2") {
      btn1.classList.remove("nav-slctd")
      btn2.classList.add("nav-slctd")
      btn3.classList.remove("nav-slctd")
+     dashboard.classList.add("hide")
+     wallet.classList.remove("hide")
+     user.classList.add("hide")
    }
    else if (butn === "3") {
      btn1.classList.remove("nav-slctd")
      btn2.classList.remove("nav-slctd")
      btn3.classList.add("nav-slctd")
+     dashboard.classList.add("hide")
+     wallet.classList.add("hide")
+     user.classList.remove("hide")
    }
  }
 
